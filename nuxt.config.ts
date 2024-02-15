709f1458a8c5f1a292c433fc06c2f96e4846c4d7
vetal7777 <jobschildren@gmail.com>
2024-02-15 03:16:55 +0200

    chore: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon'],
+  modules: ['@pinia/nuxt', 'nuxt-icon'],
   postcss: {
     plugins: {
       tailwindcss: {},
---
+++ stores/article/types/index.ts
@@ -0,0 +1,5 @@
+export type Article = {
+  title: string
+  key: string
+  youtube: string
+}
