a7e6c37170d90a3a5fb114f193a5d8a067e3eb88
vetal7777 <jobschildren@gmail.com>
2024-03-02 15:32:22 +0200

    chore: remove unused code

--- components/Ui/BaseButton/BaseButton.vue
+++ components/Ui/BaseButton/BaseButton.vue
@@ -2,7 +2,6 @@
   <button
     class="btn"
     :class="{
-      '': size === BaseButtonSize.md,
       'btn-sm': size === BaseButtonSize.sm,
       'btn-lg': size === BaseButtonSize.lg,
       'btn-xs': size === BaseButtonSize.xs
@@ -17,7 +16,5 @@
 import { BaseButtonSize } from '~/constants/global'
 import type { BaseButtonProps } from './types'
 
-withDefaults(defineProps<BaseButtonProps>(), {
-  size: BaseButtonSize.md
-})
+defineProps<BaseButtonProps>()
 </script>
