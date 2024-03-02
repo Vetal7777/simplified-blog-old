b2ba3f87d99e957d4fc42001bbb2fd9364deec21
vetal7777 <jobschildren@gmail.com>
2024-03-03 00:54:08 +0200

    feat: add new loading-spinner component

---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <Teleport to="body">
+    <Transition>
+      <div
+        v-if="isLoading"
+        class="dark:bg-main-2 fixed left-0 top-0 z-20 flex h-screen w-screen items-center justify-center bg-white"
+      >
+        <span class="du-loading du-loading-spinner du-loading-lg" />
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { useAppStore } from '~/stores/app'
+
+const appStore = useAppStore()
+
+const { isLoading } = storeToRefs(appStore)
+</script>
+
+<style scoped lang="scss">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
--- stores/app/index.ts
+++ stores/app/index.ts
@@ -1,6 +1,6 @@
 export const useAppStore = defineStore('app', () => {
   const editMode = ref(false)
-  const isLoading = ref(true)
+  const isLoading = ref(false)
 
   return { editMode, isLoading }
 })
