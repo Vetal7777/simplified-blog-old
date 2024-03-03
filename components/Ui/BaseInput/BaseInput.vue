275d2f456b2eaa2c2da2396143859cae2d7195de
vetal7777 <jobschildren@gmail.com>
2024-03-04 00:33:58 +0200

    feat: add new emit to base-input component

--- components/Ui/BaseInput/BaseInput.vue
+++ components/Ui/BaseInput/BaseInput.vue
@@ -28,13 +28,13 @@
       <div class="button-container">
         <div class="button-shell">
           <!-- Password type toggler -->
-          <template v-if="inputType.password">
+          <template v-if="inputTypeData.password">
             <!-- Show password button -->
-            <button v-if="passwordType.password" class="flex">
+            <button v-if="passwordTypeData.password" class="flex">
               <Icon
                 name="mdi:eye-off"
                 class="text-main-2 dark:text-white"
-                :size="iconSize"
+                :size="iconSizeValue"
                 @click="modelType = BaseInputType.text"
               />
             </button>
@@ -43,7 +43,7 @@
               <Icon
                 name="mdi:eye"
                 class="text-main-2 dark:text-white"
-                :size="iconSize"
+                :size="iconSizeValue"
                 @click="modelType = BaseInputType.password"
               />
             </button>
@@ -53,7 +53,7 @@
             <Icon
               name="vaadin:close"
               class="text-main-2 dark:text-white"
-              :size="iconSize"
+              :size="iconSizeValue"
             />
           </button>
         </div>
@@ -71,7 +71,7 @@
       </span>
     </div>
     <div v-if="showError" class="flex text-xs text-red">
-      {{ errors[name] }}
+      {{ errorMessage }}
     </div>
   </label>
 </template>
@@ -83,15 +83,13 @@ import { useForm } from 'vee-validate'
 import { z } from 'zod'
 import type { BaseInputProps } from './types/BaseInputProps'
 
-const emit = defineEmits(['update:modelValue'])
+const emit = defineEmits(['update:modelValue', 'error'])
 const props = withDefaults(defineProps<BaseInputProps>(), {
   modelValue: EMPTY_STRING,
   placeholder: EMPTY_STRING,
   size: BaseInputSize.md,
   type: BaseInputType.text
 })
-
-// VeeValidate
 const { errors, defineField } = useForm({
   validationSchema: toTypedSchema(
     z.object({
@@ -99,63 +97,77 @@ const { errors, defineField } = useForm({
     })
   )
 })
-
 const [validateValue, validateAttrs] = defineField(props.name, {
   validateOnModelUpdate: props.validate?.validateOnUpdate ?? true
 })
-//
 
 const modelType = ref<BaseInputType>(props.type)
 
-const model = computed({
-  get: () => props.modelValue,
-  set: (value: string) => {
-    validateValue.value = value
-    emit('update:modelValue', value)
-  }
-})
-const showError = computed(
-  () => Boolean(props.validate) && Boolean(errors.value[props.name])
+const errorMessage = computed<string | null>(
+  () => errors.value[props.name] ?? null
 )
-const showTopLabel = computed(() => props.topLeftLabel ?? props.topRightLabel)
-const showBottomLabel = computed(
-  () => props.bottomLeftLabel ?? props.bottomRightLabel
-)
-const showCloseButton = computed(() => model.value && props.clearButton)
 const inputSizeClass = computed(() => ({
-  'du-input-xs': inputSize.value.xs,
-  'du-input-sm': inputSize.value.sm,
-  'du-input-md': inputSize.value.md,
-  'du-input-lg': inputSize.value.lg
+  'du-input-xs': inputSizeData.value.xs,
+  'du-input-sm': inputSizeData.value.sm,
+  'du-input-md': inputSizeData.value.md,
+  'du-input-lg': inputSizeData.value.lg
 }))
-const inputType = computed(() => ({
+const inputTypeData = computed(() => ({
   password: props.type === BaseInputType.password,
   text: props.type === BaseInputType.text
 }))
-const inputSize = computed(() => ({
+const inputSizeData = computed(() => ({
   xs: props.size === BaseInputSize.xs,
   sm: props.size === BaseInputSize.sm,
   md: props.size === BaseInputSize.md,
   lg: props.size === BaseInputSize.lg
 }))
-const iconSize = computed(() => {
+const iconSizeValue = computed(() => {
   switch (true) {
-    case inputSize.value.xs:
+    case inputSizeData.value.xs:
       return '8'
-    case inputSize.value.sm:
+    case inputSizeData.value.sm:
       return '12'
-    case inputSize.value.md:
+    case inputSizeData.value.md:
       return '16'
-    case inputSize.value.lg:
+    case inputSizeData.value.lg:
       return '20'
   }
 })
-const passwordType = computed(() => ({
+const model = computed({
+  get: () => props.modelValue,
+  set: (value: string) => {
+    setModelValue(value)
+    onError()
+  }
+})
+const passwordTypeData = computed(() => ({
   password: modelType.value === BaseInputType.password,
   text: modelType.value === BaseInputType.text
 }))
+const showError = computed(
+  () => Boolean(props.validate) && Boolean(errorMessage.value)
+)
+const showTopLabel = computed(
+  () => Boolean(props.topLeftLabel) || Boolean(props.topRightLabel)
+)
+const showBottomLabel = computed(
+  () => Boolean(props.bottomLeftLabel) || Boolean(props.bottomRightLabel)
+)
+const showCloseButton = computed(
+  () => Boolean(model.value) && props.clearButton
+)
 
 const clearModel = () => emit('update:modelValue', EMPTY_STRING)
+const onError = () => {
+  if (props.validate && errorMessage.value) {
+    emit('error', errorMessage.value)
+  }
+}
+const setModelValue = (value: string) => {
+  validateValue.value = value
+  emit('update:modelValue', value)
+}
 </script>
 
 <style lang="scss" scoped>
