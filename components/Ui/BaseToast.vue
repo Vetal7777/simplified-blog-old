4d029f4544031992d42a7489570127ea51c14b08
vetal7777 <jobschildren@gmail.com>
2024-03-12 22:28:15 +0200

    remove: error code

--- components/Ui/BaseToast.vue
+++ components/Ui/BaseToast.vue
@@ -25,7 +25,6 @@ import { ToastType } from '~/constants/toast'
 const { $toast } = useNuxtApp()
 
 const state = computed(() => $toast.state.value)
-console.log('true')
 </script>
 
 <style scoped lang="scss">
